bebdcbb53c0d9a24ce2163156adc50acf41e8b52
ss0526100 <ss0526100@gmail.com>
2025-03-05 09:12:29 +0900

    feat:mbti api 라우터 구현

--- src/constants/httpApiEndpoint.ts
+++ src/constants/httpApiEndpoint.ts
@@ -1,24 +1,55 @@
-const HTTP_PURE_API_END_POINT = {
-  mockTestHttp: '/mock/test/http',
-  testHttp: '/test/http',
+const HTTP_PURE_STRING_API_END_POINT = {
+  mbtiChatWildCard: '/chat/mbti/*',
+  mockMbtiChatWildCard: '/mock/chat/mbti/*',
+} as const;
 
-  mbtiChat: '/chat/mbti',
-  mockMbtiChat: '/mock/chat/mbti',
-
-  mockTestWebsocket: '/mock/test/websocket',
+const HTTP_PURE_FUNCTION_API_END_POINT = {
+  mbtiChatGet: (mbti: Mbti, lastOrder: number) =>
+    '/chat/mbti/' + mbti + '?' + 'lastOrder=' + lastOrder,
+  mbtiChatPost: (mbti: Mbti) => '/chat/mbti/' + mbti,
+  mockMbtiChat: (mbti: Mbti) => '/mock/chat/mbti/' + mbti,
 } as const;
 
 // {test:import.meta.env.VITE_API_BASE_URL + '/test',...}
-const HTTP_API_END_POINT = (
+const HTTP_STRING_API_END_POINT = (
   Object.keys(
-    HTTP_PURE_API_END_POINT,
-  ) as (keyof typeof HTTP_PURE_API_END_POINT)[]
+    HTTP_PURE_STRING_API_END_POINT,
+  ) as (keyof typeof HTTP_PURE_STRING_API_END_POINT)[]
 ).reduce(
   (obj, key) => {
-    obj[key] = import.meta.env.VITE_API_BASE_URL + HTTP_PURE_API_END_POINT[key];
+    obj[key] =
+      import.meta.env.VITE_API_BASE_URL + HTTP_PURE_STRING_API_END_POINT[key];
     return obj;
   },
-  {} as { -readonly [K in keyof typeof HTTP_PURE_API_END_POINT]: string },
+  {} as {
+    -readonly [K in keyof typeof HTTP_PURE_STRING_API_END_POINT]: string;
+  },
 );
+const HTTP_FUNCTION_API_END_POINT = (
+  Object.keys(
+    HTTP_PURE_FUNCTION_API_END_POINT,
+  ) as (keyof typeof HTTP_PURE_FUNCTION_API_END_POINT)[]
+).reduce(
+  (obj, key) => {
+    const nowFn = HTTP_PURE_FUNCTION_API_END_POINT[key];
+    obj[key] = (...args: Parameters<typeof nowFn>) =>
+      import.meta.env.VITE_API_BASE_URL +
+      nowFn(
+        // @ts-expect-error 이 인자는 무조건 튜플형 인자임
+        ...args,
+      );
+    return obj;
+  },
+  {} as {
+    -readonly [K in keyof typeof HTTP_PURE_FUNCTION_API_END_POINT]: (
+      ...args: Parameters<(typeof HTTP_PURE_FUNCTION_API_END_POINT)[K]>
+    ) => string;
+  },
+);
+
+const HTTP_API_END_POINT = {
+  ...HTTP_STRING_API_END_POINT,
+  ...HTTP_FUNCTION_API_END_POINT,
+} as const;
 
 export default HTTP_API_END_POINT;
