b1d67d448fa94a07491b4d72908ffcbe7f181f2b
ss0526100 <ss0526100@gmail.com>
2025-03-05 09:12:11 +0900

    feat:mbti 라우터 구현

--- src/constants/appEndpoint.ts
+++ src/constants/appEndpoint.ts
@@ -1,7 +1,7 @@
 const APP_END_POINT = {
   main: '/',
-  testHTTP: '/test/http',
-  testWebSocket: '/test/websocket',
+  chatMbtiWildCard: '/chat/mbti/*',
+  chatMbti: (mbti: Mbti) => '/chat/mbti/' + mbti,
 };
 
 export default APP_END_POINT;
