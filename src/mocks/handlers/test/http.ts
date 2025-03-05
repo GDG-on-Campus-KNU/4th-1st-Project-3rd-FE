3cca618872098b81318da5068087b2027dc74f21
ss0526100 <ss0526100@gmail.com>
2025-03-05 09:10:13 +0900

    delete: 안쓰는 파일 주석처리

--- src/mocks/handlers/test/http.ts
+++ src/mocks/handlers/test/http.ts
@@ -1,25 +1,25 @@
-import { HttpResponse, http } from 'msw';
+// import { HttpResponse, http } from 'msw';
 
-import HTTP_API_END_POINT from '@_/constants/httpApiEndpoint';
+// import HTTP_API_END_POINT from '@_/constants/httpApiEndpoint';
 
-let responseData: BaseResponse<MessageResponse> = {
-  data: { message: 'hello' },
-};
-let statusCode: StatusCode = 200;
+// let responseData: BaseResponse<MessageResponse> = {
+//   data: { message: 'hello' },
+// };
+// let statusCode: StatusCode = 200;
 
-export const GET = http.get(HTTP_API_END_POINT.testHttp, () => {
-  return HttpResponse.json(responseData, { status: statusCode });
-});
+// export const GET = http.get(HTTP_API_END_POINT.testHttp, () => {
+//   return HttpResponse.json(responseData, { status: statusCode });
+// });
 
-export const MOCK_TEST_POST = http.post(
-  HTTP_API_END_POINT.mockTestHttp,
-  async ({ request }) => {
-    const { data, status } =
-      (await request.json()) as ResponseInMocking<MessageResponse>;
-    responseData = { data };
-    if (status) statusCode = status;
-    return HttpResponse.json({});
-  },
-);
+// export const MOCK_TEST_POST = http.post(
+//   HTTP_API_END_POINT.mockTestHttp,
+//   async ({ request }) => {
+//     const { data, status } =
+//       (await request.json()) as ResponseInMocking<MessageResponse>;
+//     responseData = { data };
+//     if (status) statusCode = status;
+//     return HttpResponse.json({});
+//   },
+// );
 
-export default [GET, MOCK_TEST_POST];
+// export default [GET, MOCK_TEST_POST];
--- src/mocks/handlers/test/websocket.ts
+++ src/mocks/handlers/test/websocket.ts
@@ -1,31 +1,31 @@
-import { HttpResponse, http, ws } from 'msw';
+// import { HttpResponse, http, ws } from 'msw';
 
-import HTTP_API_END_POINT from '@_/constants/httpApiEndpoint';
-import WEBSOCKET_API_END_POINT from '@_/constants/websocketEndpoint';
+// import HTTP_API_END_POINT from '@_/constants/httpApiEndpoint';
+// import WEBSOCKET_API_END_POINT from '@_/constants/websocketEndpoint';
 
-const socket = ws.link(WEBSOCKET_API_END_POINT.testWebsocket);
+// const socket = ws.link(WEBSOCKET_API_END_POINT.testWebsocket);
 
-const responseData = 'data';
-const statusCode: StatusCode = 200;
+// const responseData = 'data';
+// const statusCode: StatusCode = 200;
 
-let send: () => void;
-const handleOpen = socket.addEventListener('connection', ({ client }) => {
-  send = () => client.send(responseData);
-});
+// let send: () => void;
+// const handleOpen = socket.addEventListener('connection', ({ client }) => {
+//   send = () => client.send(responseData);
+// });
 
-const POST = http.post(HTTP_API_END_POINT.mockTestWebsocket, () => {
-  send();
-  return HttpResponse.json({});
-});
-// export const MOCK_TEST_POST = http.post(
-//   HTTP_API_END_POINT.mockTestHttp,
-//   async ({ request }) => {
-//     const { data, status } =
-//       (await request.json()) as ResponseInMocking<MessageResponse>;
-//     responseData = { data };
-//     if (status) statusCode = status;
-//     return HttpResponse.json({});
-//   },
-// );
+// const POST = http.post(HTTP_API_END_POINT.mockTestWebsocket, () => {
+//   send();
+//   return HttpResponse.json({});
+// });
+// // export const MOCK_TEST_POST = http.post(
+// //   HTTP_API_END_POINT.mockTestHttp,
+// //   async ({ request }) => {
+// //     const { data, status } =
+// //       (await request.json()) as ResponseInMocking<MessageResponse>;
+// //     responseData = { data };
+// //     if (status) statusCode = status;
+// //     return HttpResponse.json({});
+// //   },
+// // );
 
-export default [handleOpen, POST];
+// export default [handleOpen, POST];
