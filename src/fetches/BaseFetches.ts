2d0c16de1196103eea3b332ebe64772b58983b31
ss0526100 <ss0526100@gmail.com>
2025-03-08 22:36:11 +0900

    feat: body 타입 제네릭 추가

--- src/fetches/BaseFetches.ts
+++ src/fetches/BaseFetches.ts
@@ -6,15 +6,17 @@ interface EmptyBodyRequestInit extends RequestInit {
   method: EmptyBodyMethod;
   body?: never;
 }
-interface HasBodyRequestInit extends Omit<RequestInit, 'body'> {
+interface HasBodyRequestInit<BodyType> extends Omit<RequestInit, 'body'> {
   method?: CanHasBodyMethod;
-  body?: object;
+  body: BodyType;
 }
 
-type CustomRequestInit = EmptyBodyRequestInit | HasBodyRequestInit;
+type CustomRequestInit<BodyType> =
+  | EmptyBodyRequestInit
+  | HasBodyRequestInit<BodyType>;
 
-function isEmptyBodyRequestInit(
-  customRequestInit: CustomRequestInit,
+function isEmptyBodyRequestInit<BodyType>(
+  customRequestInit: CustomRequestInit<BodyType>,
 ): customRequestInit is EmptyBodyRequestInit {
   return !!(
     customRequestInit.method &&
@@ -23,7 +25,10 @@ function isEmptyBodyRequestInit(
 }
 const EMPTY_BODY_METHOD_LIST = ['GET', 'HEAD', 'DELETE', 'OPTIONS'];
 
-async function baseFetch(url: string, option: CustomRequestInit) {
+async function baseFetch<BodyType>(
+  url: string,
+  option: CustomRequestInit<BodyType>,
+): Promise<Response> {
   const { body, ...restOption } = option;
   const optionResult: RequestInit = restOption;
   if (!isEmptyBodyRequestInit(option) && body) {
@@ -32,41 +37,51 @@ async function baseFetch(url: string, option: CustomRequestInit) {
   return await fetch(url, optionResult);
 }
 
-async function normalizedFetch<T>(
+async function normalizedFetch<ResponseType, BodyType = unknown>(
   url: string,
-  option: CustomRequestInit,
-): Promise<T> {
-  const response = await baseFetch(url, option);
+  option: CustomRequestInit<BodyType>,
+): Promise<ResponseType> {
+  const response = await baseFetch<BodyType>(url, option);
   if (!response.ok) throw new Error('에러가 났어용~');
 
-  const json = (await response.json()) as BaseResponse<T>;
+  const json = (await response.json()) as BaseResponse<ResponseType>;
   return json.data;
 }
 
-export async function getFetch<T>(
+export async function getFetch<ResponseType>(
+  url: string,
+  option: Omit<EmptyBodyRequestInit, 'method'> = { body: undefined },
+) {
+  return normalizedFetch<ResponseType>(url, { ...option, method: 'GET' });
+}
+
+export async function deleteFetch<ResponseType>(
   url: string,
   option: Omit<EmptyBodyRequestInit, 'method'> = {},
 ) {
-  return normalizedFetch<T>(url, { ...option, method: 'GET' });
+  return normalizedFetch<ResponseType>(url, { ...option, method: 'DELETE' });
 }
 
-export async function postFetch<T>(
+export async function postFetch<BodyType, ResponseType = EmptyResponse>(
   url: string,
-  option: Omit<HasBodyRequestInit, 'method'> = {},
+  option: Omit<HasBodyRequestInit<BodyType>, 'method'> = {
+    body: undefined as BodyType,
+  },
 ) {
-  return normalizedFetch<T>(url, { ...option, method: 'POST' });
+  return normalizedFetch<ResponseType, BodyType>(url, {
+    ...option,
+    method: 'POST',
+  });
 }
 
-export async function deleteFetch<T>(
+export async function patchFetch<BodyType, ResponseType = EmptyResponse>(
   url: string,
-  option: Omit<EmptyBodyRequestInit, 'method'> = {},
+  option: Omit<HasBodyRequestInit<BodyType>, 'method'> = {
+    body: undefined as BodyType,
+  },
 ) {
-  return normalizedFetch<T>(url, { ...option, method: 'DELETE' });
-}
-
-export async function patchFetch<T>(
-  url: string,
-  option: Omit<HasBodyRequestInit, 'method'> = {},
-) {
-  return normalizedFetch<T>(url, { ...option, method: 'POST' });
+  return normalizedFetch<ResponseType, BodyType>(url, {
+    ...option,
+    method: 'PATCH',
+  });
 }
