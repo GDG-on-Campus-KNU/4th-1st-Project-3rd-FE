6a4f8d738be79443a5b41fa29bdd801206a264df
ss0526100 <ss0526100@gmail.com>
2025-03-09 20:50:02 +0900

    feat:햄버거 svg 구현

---
+++ src/components/common/svgs/HamburgerSVG.tsx
@@ -0,0 +1,38 @@
+import { SVGElementProp } from '@_/types/props';
+
+export default function HamburgerSVG(props: SVGElementProp) {
+  const { width, height, color, ...restProps } = props;
+
+  return (
+    <svg
+      width={width || '24'}
+      height={height || '25'}
+      viewBox="0 0 24 25"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...restProps}
+    >
+      <path
+        d="M23.1429 3.12488H0.857178"
+        stroke={color || '#606060'}
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M23.1429 12.1249H0.857178"
+        stroke={color || '#606060'}
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M23.1429 21.1249H0.857178"
+        stroke={color || '#606060'}
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
