063c2984766bf9e5bdc1155560c63520fb653211
ss0526100 <ss0526100@gmail.com>
2025-03-09 20:50:20 +0900

    feat:reset.css 파일 수정 적용

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { createRoot } from 'react-dom/client';
 import startMSW from '@_/mocks/index.ts';
 
 import App from './App.tsx';
-import './reset.css';
+import './styles/reset.css';
 
 await startMSW();
 createRoot(document.getElementById('root')!).render(
