def3f144c5dd7e79847381fc456db208a926b8f2
ss0526100 <ss0526100@gmail.com>
2025-03-10 18:58:36 +0900

    feat:date객체를 받아 같은 날짜인지 확인하는 함수 구현

---
+++ src/utils/checkIsSameDay.ts
@@ -0,0 +1,7 @@
+export default function checkIsSameDay(dateA: Date, dateB: Date) {
+  return (
+    dateA.getFullYear() === dateB.getFullYear() &&
+    dateA.getMonth() === dateB.getMonth() &&
+    dateA.getDate() === dateB.getDate()
+  );
+}
