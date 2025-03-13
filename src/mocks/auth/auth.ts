1d8a97913977327dba2f6d8523b00f1dddd3b817
ss0526100 <ss0526100@gmail.com>
2025-03-13 21:24:10 +0900

    feat:auth 관련 로직을 stateless하게 변경

--- src/mocks/auth/auth.ts
+++ src/mocks/auth/auth.ts
@@ -4,34 +4,32 @@ import HTTP_API_END_POINT from '@_/constants/httpApiEndpoint';
 import MOCK_CONSTANTS from '@_/constants/mock';
 
 import getCookiesStr from '../utils/getCookiesStr';
-
-let hasSession = true;
+import checkIsAuthed from './checkIsAuthed';
 
 const LOGIN_POST = http.post(HTTP_API_END_POINT.login, ({ cookies }) => {
-  if (hasSession) {
+  if (checkIsAuthed(cookies)) {
     return HttpResponse.json(
       { errorMessage: '이미 로그인되어 있음' },
       { status: 401 },
     );
   }
-  hasSession = true;
   return new HttpResponse(null, {
     headers: {
       'Set-Cookie': getCookiesStr(cookies, {
         sets: [[MOCK_CONSTANTS.cookieAuthKey, MOCK_CONSTANTS.cookieAuthValue]],
       }),
     },
+    status: 200,
   });
 });
 
-const LOGOUT_POST = http.post(HTTP_API_END_POINT.login, ({ cookies }) => {
-  if (hasSession) {
+const LOGOUT_POST = http.post(HTTP_API_END_POINT.logout, ({ cookies }) => {
+  if (!checkIsAuthed(cookies)) {
     return HttpResponse.json(
       { errorMessage: '로그인 되어있지 않음음' },
       { status: 401 },
     );
   }
-  hasSession = false;
   return new HttpResponse(null, {
     headers: {
       'Set-Cookie': getCookiesStr(cookies, {
@@ -41,11 +39,20 @@ const LOGOUT_POST = http.post(HTTP_API_END_POINT.login, ({ cookies }) => {
   });
 });
 
-const CHECK_IS_AUTHED_GET = http.get(HTTP_API_END_POINT.checkIsAuthed, () => {
-  return HttpResponse.json<CheckIsAuthedBody>(
-    { isAuthed: hasSession },
-    { status: 200 },
-  );
-});
+const CHECK_IS_AUTHED_GET = http.get(
+  HTTP_API_END_POINT.checkIsAuthed,
+  ({ cookies }) => {
+    return HttpResponse.json<BaseResponse<CheckIsAuthedBody>>(
+      {
+        data: {
+          isAuthed:
+            cookies[MOCK_CONSTANTS.cookieAuthKey] ===
+            MOCK_CONSTANTS.cookieAuthValue,
+        },
+      },
+      { status: 200 },
+    );
+  },
+);
 
 export default [LOGIN_POST, LOGOUT_POST, CHECK_IS_AUTHED_GET];
