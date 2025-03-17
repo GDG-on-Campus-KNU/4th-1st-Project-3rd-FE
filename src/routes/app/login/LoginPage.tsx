3581be4520be76367a4c6de7b28c9649cde1394e
ss0526100 <ss0526100@gmail.com>
2025-03-17 18:53:09 +0900

    feat:로그인 페이지 구현

--- src/routes/app/login/LoginPage.tsx
+++ src/routes/app/login/LoginPage.tsx
@@ -1,28 +1,60 @@
-import { useEffect } from 'react';
+import { useState } from 'react';
 
 import { useNavigate } from 'react-router-dom';
 
+import Button from '@_/components/common/Button/Button';
+import ControlledInput from '@_/components/common/Input/ControlledInput';
 import APP_END_POINT from '@_/constants/appEndpoint';
 import HTTP_API_END_POINT from '@_/constants/httpApiEndpoint';
-import { getFetch } from '@_/fetches/BaseFetches';
+import { postFetch } from '@_/fetches/BaseFetches';
+
+import styles from './LoginPage.module.css';
 
 export default function AppLoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassWord] = useState('');
+  const [errorMessage, setErrorMessage] = useState<null | string>(null);
   const navigate = useNavigate();
+  const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
-  useEffect(() => {
-    async function check() {
-      const { isAuthed } = await getFetch<CheckIsAuthedBody>(
-        HTTP_API_END_POINT.checkIsAuthed,
-      );
-      if (isAuthed) navigate(APP_END_POINT.chatMbti('ISFJ'));
-    }
-    const id = setInterval(check, 100);
-    return () => clearInterval(id);
-  }, [navigate]);
+    postFetch<LoginRequestBody>(HTTP_API_END_POINT.login, {
+      body: { email, password },
+    })
+      .then(() => {
+        navigate(APP_END_POINT.chatMbti('ISFJ'));
+      })
+      .catch(() => {
+        setErrorMessage('아이디 혹은 비밀번호가 틀렸습니다');
+      });
+  };
 
   return (
     <>
-      <span style={{ fontSize: '30px' }}>로그인하세요</span>
+      <div className={styles.description}>
+        로그인 후
+        <br />
+        이용이 가능합니다
+      </div>
+      <form className={styles.form} onSubmit={handleLogin}>
+        <ControlledInput
+          placeholder="아이디"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          isError={!!errorMessage}
+        />
+        <div className={styles['id-input']} />
+        <ControlledInput
+          placeholder="비밀번호"
+          type={'password'}
+          maxLength={30}
+          value={password}
+          onChange={(e) => setPassWord(e.target.value)}
+          isError={!!errorMessage}
+        />
+        <div className={styles['error-message']}>{errorMessage}</div>
+        <Button className={styles['login-button']}>로그인</Button>
+      </form>
     </>
   );
 }
