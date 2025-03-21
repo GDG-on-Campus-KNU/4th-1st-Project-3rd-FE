a506df01c33a4ddebcd985d5075d2794bfd281e9
ss0526100 <ss0526100@gmail.com>
2025-03-21 16:16:04 +0900

    feat:로그인 되었을 씨 isfj 페이지로 강제로 이동

---
+++ src/hooks/useNonLoginPage.tsx
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+
+import { useNavigate } from 'react-router-dom';
+
+import APP_END_POINT from '@_/constants/appEndpoint';
+import HTTP_API_END_POINT from '@_/constants/httpApiEndpoint';
+import { getFetch } from '@_/fetches/BaseFetches';
+
+export default function useNonLoginPage() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    async function checkIsAuthed() {
+      const { isAuthed } = await getFetch<CheckIsAuthedBody>(
+        HTTP_API_END_POINT.checkIsAuthed,
+      );
+
+      if (isAuthed) navigate(APP_END_POINT.chatMbti('ISFJ'));
+    }
+    checkIsAuthed();
+  }, [navigate]);
+}
