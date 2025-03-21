c9daa5da4f59b8bcc7c1a165749a4e083d9dd326
ss0526100 <ss0526100@gmail.com>
2025-03-21 13:35:06 +0900

    feat:이메일 모킹서버 구현

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,9 +2,11 @@ import { RequestHandler } from 'node_modules/msw/lib/core/handlers/RequestHandle
 import { WebSocketHandler } from 'node_modules/msw/lib/core/handlers/WebSocketHandler';
 
 import auth from '@_/mocks/handlers/auth/auth';
+import email from '@_/mocks/handlers/auth/email';
 import chatMbti from '@_/mocks/handlers/chat/mbti/[mbti]';
 
 export const handlers: Array<RequestHandler | WebSocketHandler> = [
   ...chatMbti,
   ...auth,
+  ...email,
 ];
