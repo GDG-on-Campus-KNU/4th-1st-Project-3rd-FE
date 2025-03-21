a167b1a5c110c9a82ae1f19bdb482dc3efec4ae5
ss0526100 <ss0526100@gmail.com>
2025-03-21 16:15:49 +0900

    feat:회원가입 페이지 appEndPoint 구현

--- src/constants/appEndpoint.ts
+++ src/constants/appEndpoint.ts
@@ -3,6 +3,7 @@ const APP_END_POINT = {
   login: '/login',
   chatMbtiWithSegment: '/chat/mbti/:mbti',
   chatMbti: (mbti: Mbti) => '/chat/mbti/' + mbti.toLowerCase(),
+  register: '/register',
 };
 
 export default APP_END_POINT;
