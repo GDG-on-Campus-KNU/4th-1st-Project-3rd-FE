ddec9721ef1c4bd590b2a6122f3600ff45349bcc
ss0526100 <ss0526100@gmail.com>
2025-03-25 10:53:18 +0900

    feat:StepSolidIndicator 구현

---
+++ src/components/common/SoildStepper/SolidStepIndicator.stories.tsx
@@ -0,0 +1,17 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import SolidStepIndicator from './SolidStepperIndicator';
+
+const meta = {
+  title: 'Common/SolidStepIndicator',
+  args: {
+    maxStep: 5,
+    nowStep: 1,
+  },
+  component: SolidStepIndicator,
+} satisfies Meta<typeof SolidStepIndicator>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const button: Story = {};
