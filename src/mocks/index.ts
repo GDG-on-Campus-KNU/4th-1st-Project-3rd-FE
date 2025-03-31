d33714205aeceaf9cec29be5b53307267eae7b2c
ss0526100 <ss0526100@gmail.com>
2025-03-31 17:30:06 +0900

    refactor:사용하지 않는 node msw 제거

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,7 +1,6 @@
 async function startMSW() {
   if (typeof window === 'undefined') {
-    const { server } = await import('./server');
-    server.listen();
+    return;
   } else {
     const { worker } = await import('./browser');
     await worker.start({
