0e394b6ac6f25b6e6743b3da423271b10dc0f268
ss0526100 <ss0526100@gmail.com>
2025-04-06 20:11:29 +0900

    feat:필요한 http 엔드포인트 추가

--- src/constants/httpApiEndpoint.ts
+++ src/constants/httpApiEndpoint.ts
@@ -1,5 +1,10 @@
 const HTTP_PURE_STRING_API_END_POINT = {
   mbtiChatWildCard: '/chat/mbti/*',
+  mbtiChatInitWildCard: '/chat/mbti/*/init',
+  mbtiChatCloseWildCard: '/chat/mbti/*/close',
+  mbtiChatOpenPost: '/chat/mbti/open',
+  mbtiChatOpenGet: '/chat/mbti/open',
+  recentMbtiChat: '/chat/mbti/resent',
   mockMbtiChatWildCard: '/mock/chat/mbti/*',
   checkIsAuthed: '/user/check-session',
   login: '/auth/login',
@@ -14,6 +19,8 @@ const HTTP_PURE_FUNCTION_API_END_POINT = {
   mbtiChatGet: (mbti: Mbti, startOrder: number) =>
     '/chat/mbti/' + mbti.toLowerCase() + '?' + 'startOrder=' + startOrder,
   mbtiChatPost: (mbti: Mbti) => '/chat/mbti/' + mbti.toLowerCase(),
+  mbtiChatInit: (mbti: Mbti) => '/chat/mbti/' + mbti.toLowerCase() + '/init',
+  mbtiChatClose: (mbti: Mbti) => '/chat/mbti/' + mbti.toLowerCase() + '/close',
   mockMbtiChat: (mbti: Mbti) => '/mock/chat/mbti/' + mbti.toLowerCase(),
 } as const;
 
