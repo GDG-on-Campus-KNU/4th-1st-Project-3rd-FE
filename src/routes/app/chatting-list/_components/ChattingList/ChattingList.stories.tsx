30546a852843198adc99434ec832f1eb046789ed
ss0526100 <ss0526100@gmail.com>
2025-04-16 09:56:55 +0900

    feat:채팅방 리스트 구현

---
+++ src/routes/app/chatting-list/_components/ChattingList/ChattingList.stories.tsx
@@ -0,0 +1,45 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import ChattingList from './ChattingList';
+
+const meta = {
+  title: 'Chat/ChattingList',
+  component: ChattingList,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    initChat: { action: 'initChat' },
+    deleteChat: { action: 'deleteChat' },
+    onChattingRoomClick: { action: 'onChattingRoomClick' },
+  },
+} satisfies Meta<typeof ChattingList>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    chattingPreviews: [
+      {
+        mbti: 'ENTP',
+        lastMessage: '안녕하세요!',
+        isViewed: false,
+      },
+      {
+        mbti: 'INTJ',
+        lastMessage: '코드 리뷰 부탁드립니다.',
+        isViewed: true,
+      },
+      {
+        mbti: 'ISTP',
+        lastMessage: '코드 리뷰 부탁드립니다.',
+        isViewed: true,
+      },
+    ],
+    initChat: (mbti) => alert('initChat ' + mbti),
+    deleteChat: (mbti) => alert('deleteChat ' + mbti),
+    onChattingRoomClick: (mbti) => alert('onChattingRoomClick ' + mbti),
+  },
+};
