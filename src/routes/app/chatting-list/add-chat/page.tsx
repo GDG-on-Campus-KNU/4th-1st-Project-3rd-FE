b7f69099cf18870fd188000a056d5184e0266a04
ss0526100 <ss0526100@gmail.com>
2025-04-17 19:09:27 +0900

    feat:모달 추가 완료

--- src/routes/app/chatting-list/add-chat/page.tsx
+++ src/routes/app/chatting-list/add-chat/page.tsx
@@ -1,16 +1,70 @@
 import APP_END_POINT from '@_/constants/appEndpoint';
+import Button from '@_/components/common/Button/Button';
 import HTTP_API_END_POINT from '@_/constants/httpApiEndpoint';
 import MbtiList from './_components/MbtiList/MbtiList';
+import { Modal } from '@_/components/common/Modal/Modal';
 import SolidArrowHeadSVG from '@_/components/common/svgs/SolidArrowHeadSVG';
 import SonaWithBlur from '@_/components/common/SonaWithBlur/SonaWithBlur';
 import { postFetch } from '@_/fetches/BaseFetches';
 import styles from './page.module.css';
 import useClosedMbti from './_hooks/useClosedMbti';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+
+const ModalContent = ({
+  mbti,
+  onClose,
+}: {
+  mbti: Mbti;
+  onClose: () => void;
+}) => {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <p className={styles['modal-title']}>{mbti} 채팅방을 추가할까요?</p>
+      <p className={styles['modal-description']}>
+        MBTI당 하나의 채팅방을 추가할 수 있어요
+      </p>
+      <div className={styles['button-container']}>
+        <div className={styles['button-wrapper']}>
+          <Button
+            onClick={onClose}
+            style={{
+              backgroundColor: '#dedede',
+              color: '#fff',
+              border: 'none',
+            }}
+          >
+            그만두기
+          </Button>
+        </div>
+        <div className={styles['button-wrapper']}>
+          <Button
+            onClick={async () => {
+              await postFetch<ChatMbtiOpenPostRequestBody>(
+                HTTP_API_END_POINT.mbtiChatOpenPost,
+                { body: { mbti } },
+              );
+              navigate(APP_END_POINT.chatMbti(mbti));
+              onClose();
+            }}
+            style={{
+              border: 'none',
+            }}
+          >
+            추가하기
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default function AppAddChatPage() {
   const navigate = useNavigate();
   const closedMbti = useClosedMbti();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedMbti, setSelectedMbti] = useState<Mbti | null>(null);
 
   return (
     <>
@@ -26,12 +80,9 @@ export default function AppAddChatPage() {
         {closedMbti && (
           <MbtiList
             mbtiList={closedMbti}
-            addMbti={async (mbti) => {
-              await postFetch<ChatMbtiOpenPostRequestBody>(
-                HTTP_API_END_POINT.mbtiChatOpenPost,
-                { body: { mbti } },
-              );
-              navigate(APP_END_POINT.chatMbti(mbti));
+            addMbti={(mbti) => {
+              setIsModalOpen(true);
+              setSelectedMbti(mbti);
             }}
           />
         )}
@@ -44,6 +95,14 @@ export default function AppAddChatPage() {
           </div>
         )}
       </section>
+      {isModalOpen && selectedMbti && (
+        <Modal onClose={() => setIsModalOpen(false)}>
+          <ModalContent
+            mbti={selectedMbti}
+            onClose={() => setIsModalOpen(false)}
+          />
+        </Modal>
+      )}
     </>
   );
 }
