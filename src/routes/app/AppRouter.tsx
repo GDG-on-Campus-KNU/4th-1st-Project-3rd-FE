fd0e097b25ee28f5f6e179e5c2ccbef33f86d261
ss0526100 <ss0526100@gmail.com>
2025-04-21 17:23:36 +0900

    feat:노말 에러바운더리 페이지 적용

--- src/routes/app/AppRouter.tsx
+++ src/routes/app/AppRouter.tsx
@@ -5,6 +5,7 @@ import NetworkErrorBoundary from '@_/fetches/NetworkErrorBoundary';
 import MainLayout from '@_/routes/app/layout';
 import AppMainPage from '@_/routes/app/page';
 
+import NormalErrorBoundary from './NormalErrorBoundary';
 import AppChatMbtiPage from './chat/mbti/[mbti]/ChatMbtiPage';
 import AppChattingListPage from './chatting-list/AppChattingListPage';
 import AppAddChatPage from './chatting-list/add-chat/page';
@@ -15,34 +16,34 @@ import AppRegisterSuccessPage from './register/success/AppRegisterSuccessPage';
 export default function AppRouter() {
   return (
     <MainLayout>
-      <NetworkErrorBoundary>
-        <Routes>
-          <Route path={APP_END_POINT.main} element={<AppMainPage />} />
-          <Route
-            path={APP_END_POINT.chatMbtiWithSegment}
-            element={<AppChatMbtiPage />}
-          />
-          <Route path={APP_END_POINT.login} element={<LoginPage />} />
-          <Route path={APP_END_POINT.register} element={<AppRegisterPage />} />
-          <Route
-            path={APP_END_POINT.registerSuccess}
-            element={<AppRegisterSuccessPage />}
-          />
-          <Route
-            path={APP_END_POINT.chattingList}
-            element={<AppChattingListPage />}
-          />
-          <Route
-            path={APP_END_POINT.chattingListAdd}
-            element={<AppAddChatPage />}
-          />
-          {/* <Route path={APP_END_POINT.testHTTP} element={<AppTestHttpPage />} />
-        <Route
-          path={APP_END_POINT.testWebSocket}
-          element={<AppTestWebsocketPage />}
-          /> */}
-        </Routes>
-      </NetworkErrorBoundary>
+      <NormalErrorBoundary>
+        <NetworkErrorBoundary>
+          <Routes>
+            <Route path={APP_END_POINT.main} element={<AppMainPage />} />
+            <Route
+              path={APP_END_POINT.chatMbtiWithSegment}
+              element={<AppChatMbtiPage />}
+            />
+            <Route path={APP_END_POINT.login} element={<LoginPage />} />
+            <Route
+              path={APP_END_POINT.register}
+              element={<AppRegisterPage />}
+            />
+            <Route
+              path={APP_END_POINT.registerSuccess}
+              element={<AppRegisterSuccessPage />}
+            />
+            <Route
+              path={APP_END_POINT.chattingList}
+              element={<AppChattingListPage />}
+            />
+            <Route
+              path={APP_END_POINT.chattingListAdd}
+              element={<AppAddChatPage />}
+            />
+          </Routes>
+        </NetworkErrorBoundary>
+      </NormalErrorBoundary>
     </MainLayout>
   );
 }
