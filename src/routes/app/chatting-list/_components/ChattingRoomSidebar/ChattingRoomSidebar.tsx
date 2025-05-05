d7b2359b5aff5cd2d676b35a3a4c19ab2ff3a5de
ss0526100 <ss0526100@gmail.com>
2025-05-05 19:28:54 +0900

    feat:사이드바 확장 가능하게 변경

--- src/routes/app/chatting-list/_components/ChattingRoomSidebar/ChattingRoomSidebar.tsx
+++ src/routes/app/chatting-list/_components/ChattingRoomSidebar/ChattingRoomSidebar.tsx
@@ -1,20 +1,26 @@
+import { HTMLProps } from 'react';
+
 import AtSVG from '@_/components/common/svgs/AtSvg';
 import GoOutSVG from '@_/components/common/svgs/GoOutSvg';
 import PeopleCancelSVG from '@_/components/common/svgs/PeopleCancelSVG';
 
 import styles from './ChattingRoomSidebar.module.css';
 
-interface ChattingRoomSidebarProps {
+interface ChattingRoomSidebarProps extends HTMLProps<HTMLDivElement> {
   email: string | null;
   logout: () => void;
   cancel: () => void;
 }
 
 export default function ChattingRoomSidebar(props: ChattingRoomSidebarProps) {
-  const { email, logout, cancel } = props;
+  const { email, logout, cancel, className, style, ...restProps } = props;
 
   return (
-    <section className={[styles.sidebar].join(' ')}>
+    <section
+      className={[styles.sidebar, className].join(' ')}
+      style={{ ...style }}
+      {...restProps}
+    >
       <div className={styles.header}>
         <AtSVG className={styles.svg} />
         <span>{email}</span>
