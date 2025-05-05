18e473077f636a36a2f77e3bc8fb93cbf98cf622
ss0526100 <ss0526100@gmail.com>
2025-05-05 21:08:22 +0900

    feat:로그아웃 모달 구현

--- src/routes/app/chatting-list/AppChattingListPage.tsx
+++ src/routes/app/chatting-list/AppChattingListPage.tsx
@@ -9,14 +9,14 @@ import SolidPlusSVG from '@_/components/common/svgs/SolidPlusSVG';
 import SONASvg from '@_/components/common/svgs/sona/SONASvg';
 import APP_END_POINT from '@_/constants/appEndpoint';
 import HTTP_API_END_POINT from '@_/constants/httpApiEndpoint';
-import { deleteFetch, getFetch } from '@_/fetches/BaseFetches';
+import { deleteFetch, getFetch, postFetch } from '@_/fetches/BaseFetches';
 import useEmail from '@_/hooks/useEmail';
 
 import styles from './AppChattingListPage.module.css';
 import ChattingList from './_components/ChattingList/ChattingList';
 import ChattingRoomSidebar from './_components/ChattingRoomSidebar/ChattingRoomSidebar';
 
-const ModalContent = ({
+const ChatManageModalContent = ({
   mbti,
   type,
   onClose,
@@ -74,6 +74,47 @@ const ModalContent = ({
   );
 };
 
+const LogoutModalContent = ({
+  onClose,
+  onLogout,
+}: {
+  onClose: () => void;
+  onLogout: () => void;
+}) => {
+  return (
+    <div>
+      <p className={styles['modal-title']}>로그아웃</p>
+      <p className={styles['modal-description']}>정말 로그아웃 하시겠어요?</p>
+      <div className={styles['button-container']}>
+        <div className={styles['button-wrapper']}>
+          <Button
+            onClick={onClose}
+            style={{
+              backgroundColor: '#dedede',
+              color: '#fff',
+              border: 'none',
+            }}
+          >
+            뒤로가기
+          </Button>
+        </div>
+        <div className={styles['button-wrapper']}>
+          <Button
+            onClick={onLogout}
+            style={{
+              backgroundColor: '#ff321b',
+              color: '#fff',
+              border: 'none',
+            }}
+          >
+            로그아웃
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const getSideBarStyle = (isMoved: boolean, isOpen: boolean) => {
   if (!isMoved) return styles.init;
   if (isOpen) return styles.open;
@@ -87,10 +128,11 @@ export default function AppChattingListPage() {
   const navigate = useNavigate();
   const [lastMbti, setLastMbti] = useState<Mbti | null>(null);
   const [lastType, setLastType] = useState<'close' | 'reset' | null>(null);
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isChatManageModalOpen, setIsChatManageModalOpen] = useState(false);
+  const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
   const [isSidebarOpened, setIsSidebarOpened] = useState(false);
   const [isSidebarMoved, setIsSidebarMoved] = useState(false);
-  const { email } = useEmail();
+  const { email, resetEmail } = useEmail();
   const mainContainerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -130,74 +172,101 @@ export default function AppChattingListPage() {
     setIsSidebarMoved(true);
     setIsSidebarOpened((prev) => !prev);
   }, []);
-  return (
-    <div
-      className={[
-        styles.container,
-        getSideBarStyle(isSidebarMoved, isSidebarOpened),
-      ].join(' ')}
-      ref={mainContainerRef}
-    >
-      <ChattingRoomSidebar email={email} logout={() => {}} cancel={() => {}} />
 
-      <div className={styles['main-container']}>
-        {isSidebarOpened && (
-          <div
-            className={styles.dimmer}
-            onClick={() => setIsSidebarOpened(false)}
-          />
-        )}
-        <header className={styles.header}>
-          <HamburgerSVG onClick={handleHamburgerClick} />
-          채팅
-        </header>
-        <section className={styles.section}>
-          <button className={styles['add-button']} onClick={handleAddClick}>
-            <p className={styles['add-button-description']}>채팅방 추가하기</p>
-            <SolidPlusSVG className={styles['plus-icon']} />
-          </button>
-          {!isLoading && chattingList.length === 0 && (
-            <div className={styles['empty-container']}>
-              <div className={styles['sona-container']}>
-                <div className={styles.blur} />
-                <SONASvg type="sleep" className={styles.sona} />
-              </div>
-              <p className={styles['sona-text']}>
-                채팅방을 추가하여 SONA와 함께
-                <br />
-                MBTI채팅을 시작하세요!
+  const handleLogout = useCallback(async () => {
+    await postFetch(HTTP_API_END_POINT.logout);
+    resetEmail();
+    setIsLogoutModalOpen(false);
+    navigate(APP_END_POINT.main);
+  }, [resetEmail, navigate]);
+
+  return (
+    <>
+      <div
+        className={[
+          styles.container,
+          getSideBarStyle(isSidebarMoved, isSidebarOpened),
+        ].join(' ')}
+        ref={mainContainerRef}
+      >
+        <ChattingRoomSidebar
+          email={email}
+          logout={() => setIsLogoutModalOpen(true)}
+          cancel={() => {}}
+        />
+
+        <div className={styles['main-container']}>
+          {isSidebarOpened && (
+            <div
+              className={styles.dimmer}
+              onClick={() => setIsSidebarOpened(false)}
+            />
+          )}
+          <header className={styles.header}>
+            <HamburgerSVG onClick={handleHamburgerClick} />
+            채팅
+          </header>
+          <section className={styles.section}>
+            <button className={styles['add-button']} onClick={handleAddClick}>
+              <p className={styles['add-button-description']}>
+                채팅방 추가하기
               </p>
-            </div>
+              <SolidPlusSVG className={styles['plus-icon']} />
+            </button>
+            {!isLoading && chattingList.length === 0 && (
+              <div className={styles['empty-container']}>
+                <div className={styles['sona-container']}>
+                  <div className={styles.blur} />
+                  <SONASvg type="sleep" className={styles.sona} />
+                </div>
+                <p className={styles['sona-text']}>
+                  채팅방을 추가하여 SONA와 함께
+                  <br />
+                  MBTI채팅을 시작하세요!
+                </p>
+              </div>
+            )}
+            {!isLoading && chattingList.length > 0 && (
+              <ChattingList
+                chattingPreviews={chattingList}
+                onChattingRoomClick={(mbti) =>
+                  navigate(APP_END_POINT.chatMbti(mbti))
+                }
+                initChat={(mbti) => {
+                  setLastMbti(mbti);
+                  setLastType('reset');
+                  setIsChatManageModalOpen(true);
+                }}
+                deleteChat={(mbti) => {
+                  setLastMbti(mbti);
+                  setLastType('close');
+                  setIsChatManageModalOpen(true);
+                }}
+              />
+            )}
+          </section>
+          {isChatManageModalOpen && lastMbti && lastType && (
+            <Modal onClose={() => setIsChatManageModalOpen(false)}>
+              <ChatManageModalContent
+                mbti={lastMbti}
+                type={lastType}
+                onClose={() => setIsChatManageModalOpen(false)}
+              />
+            </Modal>
           )}
-          {!isLoading && chattingList.length > 0 && (
-            <ChattingList
-              chattingPreviews={chattingList}
-              onChattingRoomClick={(mbti) =>
-                navigate(APP_END_POINT.chatMbti(mbti))
-              }
-              initChat={(mbti) => {
-                setLastMbti(mbti);
-                setLastType('reset');
-                setIsModalOpen(true);
-              }}
-              deleteChat={(mbti) => {
-                setLastMbti(mbti);
-                setLastType('close');
-                setIsModalOpen(true);
-              }}
-            />
-          )}
-        </section>
-        {isModalOpen && lastMbti && lastType && (
-          <Modal onClose={() => setIsModalOpen(false)}>
-            <ModalContent
-              mbti={lastMbti}
-              type={lastType}
-              onClose={() => setIsModalOpen(false)}
-            />
-          </Modal>
-        )}
+        </div>
       </div>
-    </div>
+      {isLogoutModalOpen && (
+        <Modal
+          onClose={() => setIsLogoutModalOpen(false)}
+          dimmerColor="transparent"
+        >
+          <LogoutModalContent
+            onClose={() => setIsLogoutModalOpen(false)}
+            onLogout={handleLogout}
+          />
+        </Modal>
+      )}
+    </>
   );
 }
