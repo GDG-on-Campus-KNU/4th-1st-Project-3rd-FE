7beca333611be66e0895d8c22e2430be69d76394
ss0526100 <ss0526100@gmail.com>
2025-05-05 18:56:04 +0900

    feat: 사이드바 컴포넌트 구현

---
+++ src/routes/app/chatting-list/_components/ChattingRoomSidebar/ChattingRoomSidebar.tsx
@@ -0,0 +1,37 @@
+import AtSVG from '@_/components/common/svgs/AtSvg';
+import GoOutSVG from '@_/components/common/svgs/GoOutSvg';
+import PeopleCancelSVG from '@_/components/common/svgs/PeopleCancelSVG';
+
+import styles from './ChattingRoomSidebar.module.css';
+
+interface ChattingRoomSidebarProps {
+  email: string | null;
+  logout: () => void;
+  cancel: () => void;
+}
+
+export default function ChattingRoomSidebar(props: ChattingRoomSidebarProps) {
+  const { email, logout, cancel } = props;
+
+  return (
+    <section className={[styles.sidebar].join(' ')}>
+      <div className={styles.header}>
+        <AtSVG className={styles.svg} />
+        <span>{email}</span>
+      </div>
+      <div>
+        <div className={styles['bottom-box']} onClick={logout}>
+          <GoOutSVG className={[styles.svg, styles.grey].join(' ')} />
+          <span>로그아웃</span>
+        </div>
+        <div
+          className={[styles['bottom-box'], styles.red].join(' ')}
+          onClick={cancel}
+        >
+          <PeopleCancelSVG className={styles.svg} />
+          <span>회원탈퇴</span>
+        </div>
+      </div>
+    </section>
+  );
+}
