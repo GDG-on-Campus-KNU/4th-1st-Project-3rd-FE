dd31045ba5e181869164ff11f26b763be6eb791e
ss0526100 <ss0526100@gmail.com>
2025-05-07 18:14:04 +0900

    feat:채팅방 추가 로딩 버튼 구현

--- src/routes/app/chatting-list/add-chat/page.tsx
+++ src/routes/app/chatting-list/add-chat/page.tsx
@@ -22,6 +22,7 @@ const ModalContent = ({
   onClose: () => void;
 }) => {
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
   return (
     <div>
       <p className={styles['modal-title']}>{mbti} 채팅방을 추가할까요?</p>
@@ -32,11 +33,16 @@ const ModalContent = ({
         <div className={styles['button-wrapper']}>
           <Button
             onClick={onClose}
-            style={{
-              backgroundColor: '#dedede',
-              color: '#fff',
-              border: 'none',
-            }}
+            style={
+              isLoading
+                ? {}
+                : {
+                    backgroundColor: '#dedede',
+                    color: '#fff',
+                    border: 'none',
+                  }
+            }
+            isLoading={isLoading}
             thin
           >
             그만두기
@@ -45,16 +51,28 @@ const ModalContent = ({
         <div className={styles['button-wrapper']}>
           <Button
             onClick={async () => {
-              await postFetch<ChatMbtiOpenPostRequestBody>(
-                HTTP_API_END_POINT.mbtiChatOpenPost,
-                { body: { mbti } },
-              );
+              setIsLoading(true);
+              try {
+                await postFetch<ChatMbtiOpenPostRequestBody>(
+                  HTTP_API_END_POINT.mbtiChatOpenPost,
+                  { body: { mbti } },
+                );
+              } catch (_) {
+                alert('채팅방 추가에 실패하였습니다.');
+                setIsLoading(false);
+              }
+              setIsLoading(false);
               navigate(APP_END_POINT.chatMbti(mbti));
               onClose();
             }}
-            style={{
-              border: 'none',
-            }}
+            style={
+              isLoading
+                ? {}
+                : {
+                    border: 'none',
+                  }
+            }
+            isLoading={isLoading}
             thin
           >
             추가하기
