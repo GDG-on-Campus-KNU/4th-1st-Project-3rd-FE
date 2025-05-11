1f88f0fff70f491ffecd59848f80289658d1de1a
ss0526100 <ss0526100@gmail.com>
2025-05-11 20:45:55 +0900

    fix:메세지 보내고 나서 사이즈 원상복귀 하도록 구현현

--- src/routes/app/chat/mbti/[mbti]/_component/MessageTextArea/MessageTextArea.tsx
+++ src/routes/app/chat/mbti/[mbti]/_component/MessageTextArea/MessageTextArea.tsx
@@ -1,4 +1,11 @@
-import { HTMLProps, useCallback, useEffect, useRef, useState } from 'react';
+import {
+  HTMLProps,
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from 'react';
 
 import SolidArrowSVG from '@_/components/common/svgs/SolidArrowSVG';
 
@@ -86,19 +93,21 @@ export default function MessageTextArea(props: MessageTextAreaProps) {
     (e: React.ChangeEvent<HTMLTextAreaElement>) => {
       setValue(e.target.value);
 
-      const el = textAreaRef.current;
-      if (el) {
-        el.style.height = 'auto';
-        const height = Math.min(el.scrollHeight, maxTextAreaHeight);
-        el.style.height = height + 'px';
-      }
       if (onValueChange) {
         onValueChange(value);
       }
     },
-    [maxTextAreaHeight, onValueChange, value],
+    [onValueChange, value],
   );
 
+  useLayoutEffect(() => {
+    const el = textAreaRef.current;
+    if (el) {
+      el.style.height = 'auto';
+      const height = Math.min(el.scrollHeight, maxTextAreaHeight);
+      el.style.height = height + 'px';
+    }
+  }, [value, maxTextAreaHeight]);
   return (
     <form
       onSubmit={handleSubmit}
