cc8b699f9b87f9d327feb292ea75b6985f2f6c7c
ss0526100 <ss0526100@gmail.com>
2025-06-10 07:59:04 +0900

    feat:스텝 5 mbti 설명 추가

--- src/routes/app/analysis/face/_step/5/page.tsx
+++ src/routes/app/analysis/face/_step/5/page.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import Button from '@_/components/common/Button/Button';
 import SonaWithBlur from '@_/components/common/SonaWithBlur/SonaWithBlur';
 import APP_END_POINT from '@_/constants/appEndpoint';
+import MBTI_DESCRIPTION from '@_/constants/mbtiDescription';
 import MBTI_NICKNAME from '@_/constants/mbtiNickname';
 
 import styles from './page.module.css';
@@ -24,6 +25,11 @@ export default function AnalysisFaceStep5({
       </span>
       <div className={styles.svg}>
         <SonaWithBlur type={mbti} blurMultiple={1.3} />
+      </div>
+      <div className={styles['mbti-description']}>
+        {MBTI_DESCRIPTION[mbti].map((description, index) => (
+          <p key={index}>{description}</p>
+        ))}
       </div>
       <div className={styles['button-container']}>
         <Button onClick={() => navigate(APP_END_POINT.login)}>
