05e37b2a7e58b1317b63d1697278c4cfd8670629
ss0526100 <ss0526100@gmail.com>
2025-06-09 16:30:05 +0900

    delete:사용하지 않는 파일 삭제

--- src/mocks/handlers/auth/checkIsAuthed.ts
+++
@@ -1,7 +0,0 @@
-import MOCK_CONSTANTS from '@_/constants/mock';
-
-export default function checkIsAuthed(cookies: Record<string, string>) {
-  return (
-    cookies[MOCK_CONSTANTS.cookieAuthKey] === MOCK_CONSTANTS.cookieAuthValue
-  );
-}
