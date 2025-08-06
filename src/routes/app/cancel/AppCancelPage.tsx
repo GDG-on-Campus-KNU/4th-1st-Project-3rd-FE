f24c0551cd829c79a5b874164c112e654f0cc62a
ss0526100 <ss0526100@gmail.com>
2025-08-06 18:26:12 +0900

    feat:앱 탈퇴를 mutation으로 구현

--- src/routes/app/cancel/AppCancelPage.tsx
+++ src/routes/app/cancel/AppCancelPage.tsx
@@ -1,6 +1,6 @@
-import { useCallback, useState } from 'react';
+import { useState } from 'react';
 
-import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 
 import Button from '@_/components/common/Button/Button';
@@ -14,27 +14,26 @@ import profileQueryBases from '@_/remote/profileQueryBase';
 
 import styles from './AppCancelPage.module.css';
 
+const useCancelMutation = () => {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  return useMutation({
+    mutationFn: () => deleteFetch(HTTP_API_END_POINT.cancelAccount),
+    onSuccess: () => {
+      queryClient.invalidateQueries(profileQueryBases.email());
+      navigate(APP_END_POINT.main);
+    },
+  });
+};
+
 export default function AppCancelPage() {
   const { data: email } = useQuery(profileQueryBases.email());
-  const queryClient = useQueryClient();
   const [typedEmail, setTypedEmail] = useState('');
-  const [isCancelSending, setIsCancelSending] = useState(false);
   const navigate = useNavigate();
 
   const canCancel = email === typedEmail;
 
-  const handleCancel = useCallback(async () => {
-    setIsCancelSending(true);
-    try {
-      await deleteFetch(HTTP_API_END_POINT.cancelAccount);
-    } catch (_) {
-      setIsCancelSending(false);
-      return;
-    }
-    setIsCancelSending(false);
-    queryClient.invalidateQueries(profileQueryBases.email());
-    navigate(APP_END_POINT.main);
-  }, [navigate, queryClient]);
+  const { mutate: cancel, isPending: isCancelPending } = useCancelMutation();
 
   return (
     <>
@@ -68,24 +67,24 @@ export default function AppCancelPage() {
         />
         <div className={styles['button-container']}>
           <Button
-            className={isCancelSending ? '' : styles['grey-button']}
+            className={isCancelPending ? '' : styles['grey-button']}
             onClick={() => navigate(APP_END_POINT.chattingList)}
             thin
-            isLoading={isCancelSending}
+            isLoading={isCancelPending}
           >
             취소
           </Button>
           <Button
             isValid={canCancel}
             className={
-              isCancelSending
+              isCancelPending
                 ? ''
                 : canCancel
                   ? styles.red
                   : styles['grey-button']
             }
-            onClick={handleCancel}
-            isLoading={isCancelSending}
+            onClick={() => cancel()}
+            isLoading={isCancelPending}
             thin
           >
             회원탈퇴
